3ce20c7499b5cdba154ef7f0f4fb2f120706de9e
shuvo sutradhor <shuvosd3837@gmail.com>
2024-01-06 00:48:39 +0600

    added coffees

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
 
 function App() {
+
+  const coffees = useLoaderData();
   
   return (
     <>
--- src/Componants/AddCoffee.jsx
+++ src/Componants/AddCoffee.jsx
@@ -1,4 +1,4 @@
-
+import Swal from 'sweetalert2';
 
 const AddCoffee = () => {
     const handleAddCoffee = event => {
@@ -25,8 +25,17 @@ const AddCoffee = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                console.log(data);
+                if(data.insertedId){
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Uesr added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                      })
+                }
             })
+
     }
 
 
